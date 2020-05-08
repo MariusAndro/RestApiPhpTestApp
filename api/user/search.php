39d77ea0db8a3e17b4b7a434495700341afbd569
marius <marius@marius-ThinkPad-T430>
2020-05-08 13:28:21 +0300

    Adding updated files

--- api/user/search.php
+++ api/user/search.php
@@ -1,36 +1,37 @@
 <?php
-//required headers
-header("Access-Control-Allow-Orifin");
+// required headers
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
+//include database and object files
 include_once '../config/database.php';
+//include_once '../config/core.php';
 include_once '../objects/users.php';
 
 //instantiate database and user object
 $database = new Database();
 $db = $database->getConnection();
-
-//initialize object
 $user = new User($db);
 
-//query users
+//get keywords
 
-$stmt = $user->read();
-//print_r($stmt);
+$keywords=isset($_GET["s"]) ? $_GET["s"] : "";
+
+//query products
+$stmt = $user->search($keywords);
 $num = $stmt->num_rows;
 
-//check if there is more than 1 row
+//check if more than 0 records
+//var_dump($num);
 if($num>0){
-    
-
+    //users array
     $users_arr = array();
-    $users_arr["records"] =array();
+    $users_arr["records"]=array();
     while ($row = $stmt->fetch_assoc()){
         //extract row
         extract($row);
         //print_r($row);
-        $user_ind = array(
-            "User_ID" => $row["User_ID"],
+        $user_ind = array(            
             "personal_id_number" => $row["personal_id_number"],
             "First_name" => $row["First_Name"],
             "Last_name" => $row["Last_Name"],
@@ -46,15 +47,15 @@ if($num>0){
         
         //show users data in json format
         echo json_encode($user_ind);
+        }
     }
-}
-else {
+    else {
     //set response code to 404
     http_response_code(404);
     
     //tell the user no users found
     echo json_encode(
             array("message" => "No users found."));
-}
+    }
 
 ?>
